7b2d698801cd3d5659587e684d876ce771b39fc0
John Carruthers <johnc@frag-labs.com>
2017-10-12 14:22:56 -0500

    DELETE support.

---
+++ Silk.Data.SQL.Base.Tests/DeleteTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Silk.Data.SQL.Expressions;
+
+namespace Silk.Data.SQL.Base.Tests
+{
+	[TestClass]
+	public class DeleteTests
+	{
+		private TestQueryConverter _queryConverter = new TestQueryConverter();
+
+		[TestMethod]
+		public void DeleteWithoutWhere()
+		{
+			var sqlQuery = _queryConverter.ConvertToQuery(
+				QueryExpression.Delete(
+					QueryExpression.Table("TestTable")
+				));
+			Assert.AreEqual("DELETE FROM [TestTable]", sqlQuery.SqlText);
+		}
+
+		[TestMethod]
+		public void DeleteWithWhere()
+		{
+			var sqlQuery = _queryConverter.ConvertToQuery(
+				QueryExpression.Delete(
+					QueryExpression.Table("TestTable"),
+					QueryExpression.Compare(QueryExpression.Column("Column1"), ComparisonOperator.AreEqual, QueryExpression.Column("Column2"))
+				));
+			Assert.AreEqual("DELETE FROM [TestTable] WHERE ([Column1] = [Column2])", sqlQuery.SqlText);
+		}
+	}
+}
