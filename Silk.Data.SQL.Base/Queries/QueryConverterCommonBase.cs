75e930a88e566497cfd02e420daece6d76bfae9b
John Carruthers <johnc@frag-labs.com>
2017-10-15 20:39:12 -0500

    Sigh.

--- Silk.Data.SQL.Base/Queries/QueryConverterCommonBase.cs
+++ Silk.Data.SQL.Base/Queries/QueryConverterCommonBase.cs
@@ -8,10 +8,10 @@ namespace Silk.Data.SQL.Queries
 	public abstract class QueryConverterCommonBase : IQueryConverter
 	{
 		protected abstract string ProviderName { get; }
-		protected abstract string AutoIncrementSql { get; set; }
+		protected abstract string AutoIncrementSql { get; }
 
 		protected StringBuilder Sql { get; }
-		protected QueryWriter ExpressionWriter { get; }
+		protected QueryWriter ExpressionWriter { get; set; }
 
 		public QueryConverterCommonBase()
 		{
